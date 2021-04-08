48fbc986dc8b85c5883bde9800fc4565a320a88a
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-04-08 00:14:40 -0400

    Remove white space and console logs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavorites from './components/AddFavorites';
 import RemoveFavorites from './components/RemoveFavorites';
-// console.log(process.env.REACT_APP_API_KEY)
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -27,39 +26,32 @@ const App = () => {
     getMovieRequest(searchValue);
   }, [searchValue]);
   useEffect(() => {
-		const movieFavorites = JSON.parse(
-			localStorage.getItem('react-movie-app-favorites')
-		);
+    const movieFavorites = JSON.parse(
+      localStorage.getItem('react-movie-app-favorites')
+    );
 
-		if (movieFavorites) {
-			setFavorites(movieFavorites);
-		}
-	}, []);
+    if (movieFavorites) {
+      setFavorites(movieFavorites);
+    }
+  }, []);
 
-	const saveToLocalStorage = (items) => {
-		localStorage.setItem('react-movie-app-favorites', JSON.stringify(items));
-	};
-  
-
-  
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favorites', JSON.stringify(items));
+  };
 
   const addFavoriteMovie = (movie) => {
     const newFavoriteList = [...favorites, movie];
     setFavorites(newFavoriteList);
     saveToLocalStorage(newFavoriteList);
-    
   };
+
   const removeFavoriteMovie = (movie) => {
     const newFavoriteList = favorites.filter(
       (favorite) => favorite.imdbID !== movie.imdbID
     );
-
     setFavorites(newFavoriteList);
     saveToLocalStorage(newFavoriteList);
   };
-
-
-
 
   return (
     <div className='container-fluid movie-app'>
