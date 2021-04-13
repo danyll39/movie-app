12ba3684387124a6ae95ba238185f65478e3aee3
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-04-12 22:44:41 -0400

    Update api key

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const [favorites, setFavorites] = useState([]);
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_API_KEY}`;
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=8a3111f4`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
