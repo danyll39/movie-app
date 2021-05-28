b8f8dbf5e6fedb02435541cf1d02a760c12e344a
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2021-05-28 01:25:48 -0400

    Minor changes

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,13 +4,13 @@ const MovieList = (props) => {
     const FavoriteComponent = props.favoriteComponent;
     return (
         <>
-            {props.movies.map((movie, index) => (
-                <div className='image-container d-flex justify-content-start m-3'>
+            {props.movies.map((movie) => (
+                <div className='image-container d-flex justify-content-start m-3' key={movie.id}>
                     <img src={movie.Poster} alt='movie'></img>
                     <div
                         onClick={() => props.handleFavoritesClick(movie)}
                         className='overlay d-flex align-items-center justify-content-center'
-                        >
+                    >
                         <FavoriteComponent />
                     </div>
                 </div>
